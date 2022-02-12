09eecc76c3041520bc2252a25dfefdd41cc38717
Alex Jackman <jackman.alex.w@gmail.com>
2022-02-12 16:42:12 -0700

    rebuilt and refactored application to utilize vuetify plugin for styling and ease of use

---
+++ 20-4-10/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app>
+    <v-main>
+      <Form/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld';
+import Form from './components/Form';
+
+export default {
+  name: 'App',
+
+  components: {
+    Form
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
